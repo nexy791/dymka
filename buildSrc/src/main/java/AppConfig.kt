ad3cfec9e318c176b8002eea3f5d23fc35753f14
Александр Некси <nexy791@gmail.com>
2023-06-25 20:12:52 +0300

    🍱 Version 2.0.1 Media fixes

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,7 +2,7 @@ object AppConfig {
     const val compileSdk = 33
     const val minSdk = 21
     const val targetSdk = 32
-    const val versionCode = 122
-    const val versionName = "2.0.1"
+    const val versionCode = 123
+    const val versionName = "2.0.2"
 
 }
